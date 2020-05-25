5d091fe8f3c0a723441d7a4e3d4b9e01e4fa6700
dinesh-cyber <dineshmandava0@gmail.com>
2020-05-25 15:03:44 +0530

    new comit

--- din_ecom/settings.py
+++ din_ecom/settings.py
@@ -81,12 +81,9 @@ WSGI_APPLICATION = 'din_ecom.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',  # dbname
-        'USER': 'postgres',  # username
-        'PASSWORD': 'postgres',  # dbpassword
-        'HOST': '127.0.0.1',
-        'PORT': '5432'}
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
 
 
