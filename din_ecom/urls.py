eaef0e5bf2dcb5802cc2a0223a768496c1b5a461
dinesh-cyber <dineshmandava0@gmail.com>
2020-05-26 14:44:37 +0530

    alert removed

--- din_ecom/urls.py
+++ din_ecom/urls.py
@@ -23,4 +23,6 @@ urlpatterns = [
     path('', include('test1.urls')),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if not settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
